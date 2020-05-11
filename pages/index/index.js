6454cec4c352d5916e0adc71b0e020b5e1ac1308
huzexuan <1870168760@qq.com>
2020-05-11 14:23:09 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,194 +3,184 @@
 const App = getApp()
 
 Page({
-  data: {
-    // banner
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    circular: true,
-    // tab
-    tab:[{
-      title:'员工餐',
-      icon:'/img/index_tab1.png',
-      url:'/pages/order_food/index?type=1'
-    },{
-      title:'回族餐',
-      icon:'/img/index_tab2.png',
-      url:'/pages/order_food/index?type=2'
-    }],
-    phone:'15032266556',
-    index: 0,
-    ethnicOutid:1,
-    ethnic:[{
-      name:'汉族',
-      id:1
-    },{
-      name:'回族',
-      id:2
-    }],
-  },
-  onLoad() {
-    let _this = this
-    App._get('v1_0_0.home/index',{},res=>{
-      _this.setData(res.data)
-    })
-  },
-  // 弹框
-  register_pop(){
-    let _this = this
-        _this.setData({
-          pop_show:true
+    data: {
+        // banner
+        indicatorDots: true,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000,
+        circular: true,
+        phone: '15032266556',
+        index: 0,
+        ethnicOutid: 1,
+        ethnic: [{
+            name: '汉族',
+            id: 1
+        }, {
+            name: '回族',
+            id: 2
+        }],
+    },
+    onLoad() {
+        let _this = this
+        App._get('v1_0_0.home/index', {}, res => {
+            _this.setData(res.data)
         })
-        //获取所有公司
-        App._get('v1_0_0.register/get_company_list',{},res=>{
-          _this.setData({
-            company:res.data.list
-          })
-        })
-    
-  },
-  onShow(){
-    let _this = this
-    _this.setData({
-      user_id:wx.getStorageSync('user_id'),
-      user_token:wx.getStorageSync('user_token')
-    })
-    App.up_courierPage()
-    // 微信Code获取
-    wx.login({
-      success: res => {
+    },
+    // 弹框
+    register_pop() {
+        let _this = this
         _this.setData({
-          wxCode:res.code
-        },()=>{
-          App._get('v1_0_0.register/is_login',{code:_this.data.wxCode},res=>{
-            if(res.code == 200){
-              _this.setData({
-                pop_show:false,
-                user_id:res.data.user_id,
-                user_token:res.data.user_token,
-              })
-              wx.setStorageSync('user_id',res.data.user_id)
-              wx.setStorageSync('user_token',res.data.user_token)
-            }else {
-              wx.removeStorageSync('user_id')
-              wx.removeStorageSync('user_token')
-              _this.setData({
-                openid:res.data.openid
-              })
+                pop_show: true
+            })
+            //获取所有公司
+        App._get('v1_0_0.register/get_company_list', {}, res => {
+            _this.setData({
+                company: res.data.list
+            })
+        })
+
+    },
+    onShow() {
+        let _this = this
+        _this.setData({
+            user_id: wx.getStorageSync('user_id'),
+            user_token: wx.getStorageSync('user_token')
+        })
+        App.up_courierPage()
+            // 微信Code获取
+        wx.login({
+            success: res => {
+                _this.setData({
+                    wxCode: res.code
+                }, () => {
+                    App._get('v1_0_0.register/is_login', { code: _this.data.wxCode }, res => {
+                        if (res.code == 200) {
+                            _this.setData({
+                                pop_show: false,
+                                user_id: res.data.user_id,
+                                user_token: res.data.user_token,
+                            })
+                            wx.setStorageSync('user_id', res.data.user_id)
+                            wx.setStorageSync('user_token', res.data.user_token)
+                        } else {
+                            wx.removeStorageSync('user_id')
+                            wx.removeStorageSync('user_token')
+                            _this.setData({
+                                openid: res.data.openid
+                            })
+                        }
+                    })
+                })
             }
-          })
         })
-      }
-    })
-    
-  },
-  //注册
-  register(){
-    let _this = this
-    const {user_Name,user_Phone,company_id,section_id,ethnicOutid,openid}=_this.data
-    if(user_Name == '' || !user_Name){
-      App.popToast('姓名不能为空')
-      return
-    }
-    if(user_Phone == ''){
-      App.popToast('手机号不能为空')
-    }else if (!(/^1[34578]\d{9}$/.test(user_Phone)) ) {
-      App.popToast('手机号格式不对')
-      return
-    }
-    if(company_id == '' || !company_id){
-      App.popToast('请选择公司')
-      return
-    }
-    if(section_id == '' || !section_id){
-      App.popToast('请选择部门')
-      return
-    }
-    App._get('v1_0_0.register/do_register',{
-      name:user_Name,
-      phone:user_Phone,
-      company_id,
-      class_id:section_id,
-      nation:ethnicOutid,
-      openid
-    },res=>{
-      if(res.code == 200){
-        App.showSuccess(res.msg,()=>{
-          _this.setData({
-            pop_show:false,
-            user_id:res.data.user_id,
-            user_token:res.data.user_token
-          })
+
+    },
+    //注册
+    register() {
+        let _this = this
+        const { user_Name, user_Phone, company_id, section_id, ethnicOutid, openid } = _this.data
+        if (user_Name == '' || !user_Name) {
+            App.popToast('姓名不能为空')
+            return
+        }
+        if (user_Phone == '') {
+            App.popToast('手机号不能为空')
+        } else if (!(/^1[34578]\d{9}$/.test(user_Phone))) {
+            App.popToast('手机号格式不对')
+            return
+        }
+        if (company_id == '' || !company_id) {
+            App.popToast('请选择公司')
+            return
+        }
+        if (section_id == '' || !section_id) {
+            App.popToast('请选择部门')
+            return
+        }
+        App._get('v1_0_0.register/do_register', {
+            name: user_Name,
+            phone: user_Phone,
+            company_id,
+            class_id: section_id,
+            nation: ethnicOutid,
+            openid
+        }, res => {
+            if (res.code == 200) {
+                App.showSuccess(res.msg, () => {
+                    _this.setData({
+                        pop_show: false,
+                        user_id: res.data.user_id,
+                        user_token: res.data.user_token
+                    })
+                })
+                wx.setStorageSync('user_id', res.data.user_id)
+                wx.setStorageSync('user_token', res.data.user_token)
+            }
         })
-        wx.setStorageSync('user_id',res.data.user_id)
-        wx.setStorageSync('user_token',res.data.user_token)
-      }
-    })
-  },
-  //拨打联系电话
-  Tap_phone(){
-    let _this = this
-    wx.makePhoneCall({
-      phoneNumber: _this.data.phone
-    })
-  },
-  //获取手机号
-  blurPhone(e){
-    let _this = this
-    _this.setData({
-      user_Phone:e.detail.value
-    })
-  },
-  // 获取姓名
-  blurname(e){
-    let _this = this
-    _this.setData({
-      user_Name:e.detail.value
-    })
-  },
-  //获取部门
-  section_bindPickerChange(e) {
-    let _this = this
-    _this.setData({
-      index: e.detail.value,
-      section_text:_this.data.section[e.detail.value].name,
-      section_id:_this.data.section[e.detail.value].id
-    })
-  },
-  has_section(){
-    App.showError("请先选择公司")
-  },
-  // 获取公司
-  company_bindPickerChange(e){
-    let _this = this
-    _this.setData({
-      index: e.detail.value,
-      company_text:_this.data.company[e.detail.value].name,
-      company_id:_this.data.company[e.detail.value].id
-    })
-    // 获取所有部门
-    App._get('v1_0_0.register/get_class_list',{
-      company_id:_this.data.company[e.detail.value].id
-    },res=>{
-      _this.setData({
-        section:res.data.list
-      })
-    })
-  },
-  // 获取民族
-  ethnic(e){
-    let _this = this
-    _this.setData({
-      ethnicOutid:e.currentTarget.id
-    })
-  },
-  onShareAppMessage: function () {
-    return {
-      title: '点击卡片立即订餐~',
-      path: '/pages/index/index' ,
-      imageUrl: '/img/share_img.jpg'
-    }
-  },
+    },
+    //拨打联系电话
+    Tap_phone() {
+        let _this = this
+        wx.makePhoneCall({
+            phoneNumber: _this.data.phone
+        })
+    },
+    //获取手机号
+    blurPhone(e) {
+        let _this = this
+        _this.setData({
+            user_Phone: e.detail.value
+        })
+    },
+    // 获取姓名
+    blurname(e) {
+        let _this = this
+        _this.setData({
+            user_Name: e.detail.value
+        })
+    },
+    //获取部门
+    section_bindPickerChange(e) {
+        let _this = this
+        _this.setData({
+            index: e.detail.value,
+            section_text: _this.data.section[e.detail.value].name,
+            section_id: _this.data.section[e.detail.value].id
+        })
+    },
+    has_section() {
+        App.showError("请先选择公司")
+    },
+    // 获取公司
+    company_bindPickerChange(e) {
+        let _this = this
+        _this.setData({
+                index: e.detail.value,
+                company_text: _this.data.company[e.detail.value].name,
+                company_id: _this.data.company[e.detail.value].id
+            })
+            // 获取所有部门
+        App._get('v1_0_0.register/get_class_list', {
+            company_id: _this.data.company[e.detail.value].id
+        }, res => {
+            _this.setData({
+                section: res.data.list
+            })
+        })
+    },
+    // 获取民族
+    ethnic(e) {
+        let _this = this
+        _this.setData({
+            ethnicOutid: e.currentTarget.id
+        })
+    },
+    onShareAppMessage: function() {
+        return {
+            title: '点击卡片立即订餐~',
+            path: '/pages/index/index',
+            imageUrl: '/img/share_img.jpg'
+        }
+    },
 })
--- pages/order_food/index.js
+++ pages/order_food/index.js
@@ -1,85 +1,122 @@
-
 const App = getApp()
 Page({
-    data:{
-        food_money:0
+    data: {
+        food_money: 0,
+        tab:[{
+            title:"员工餐",
+            icon:'/img/order_img.png',
+            id:1
+        },{
+            title:"回族餐",
+            icon:'/img/muslim.png',
+            id:2
+        }],
+        muslim:[{
+            id:1,
+            title:'用餐'
+        },{
+            id:2,
+            title:'领物品'
+        }],
+        select_tabid:1,
+        select_muslimId:1
     },
-    onLoad(options){
+    onLoad(options) {
         this.setData(options)
     },
-    onShow(){
+    onShow() {
         let _this = this
         App.up_courierPage()
-        App._get('v1_0_0.food/index',{
-            user_id:wx.getStorageSync('user_id'),
-            nation:_this.data.type
-        },res=>{
+        _this.orderList()
+        App._get('v1_0_0.user/index', {}, res => {
             _this.setData(res.data)
-            for(let i =0;i<res.data.list.length;i++){
-                if(res.data.list[i].is_selected == 1){
+        })
+
+    },
+    orderList(){
+        let _this = this
+        App._get('v1_0_0.food/index', {
+            user_id: wx.getStorageSync('user_id'),
+            nation: _this.data.select_tabid || 1
+        }, res => {
+            _this.setData(res.data)
+            for (let i = 0; i < res.data.list.length; i++) {
+                if (res.data.list[i].is_selected == 1) {
                     _this.setData({
-                        food_id:res.data.list[i].food_id
+                        food_id: res.data.list[i].food_id
                     })
                 }
             }
         })
-        App._get('v1_0_0.user/index',{},res=>{
-            _this.setData(res.data)
-        })
-        
     },
-    overtime(){
-        this.setData({overtime_pop_show:true})
+    overtime() {
+        this.setData({ overtime_pop_show: true })
     },
-    submit_order(){
-        this.setData({pop_show:true})
+    submit_order() {
+        this.setData({ pop_show: true })
     },
-    lunch_areas(e){
-        this.setData({lunch_remark:e.detail.detail.value})
+    lunch_areas(e) {
+        this.setData({ lunch_remark: e.detail.detail.value })
     },
-    dinner_areas(e){
-        this.setData({dinner_remark:e.detail.detail.value})
+    dinner_areas(e) {
+        this.setData({ dinner_remark: e.detail.detail.value })
     },
     // 申请加班餐
-    purchase(e){
-        let _this =this
-        const{lunch_remark,dinner_remark,food_id}=_this.data
-            let callback = result => {
-                App.wxPayment({
-                  payment: result.data,
-                  success: res => {
+    purchase(e) {
+        let _this = this
+        const { lunch_remark, dinner_remark, food_id } = _this.data
+        let callback = result => {
+            App.wxPayment({
+                payment: result.data,
+                success: res => {
                     App.popToast(res.msg)
                     _this.setData({
-                        overtime_pop_show:false,
-                        pop_show:false
+                        overtime_pop_show: false,
+                        pop_show: false
                     })
-                  },
-                  fail: res => {
-                      App.popToast("订单支付失败")
-                    },
-                });
-            };
-            App._get('v1_0_0.order/suborder',{
-                user_id:wx.getStorageSync('user_id'),
-                order_type:e.currentTarget.dataset.type,
-                food_id,
-                remark:e.currentTarget.dataset.type == 1?lunch_remark || '':dinner_remark || ''
-            },res=>{
-                if(res.data.user_auth == 3){
-                    App.popToast(res.msg)
-                    _this.setData({
-                        pop_show:false
-                    })
-                    wx.switchTab({
-                        url: '/pages/order/index'
-                    })
-                }else if(res.data.user_auth == 1){
-                    App._post_form('v1_0_0.wxpay/unifiedorder',{order_num:res.data.order_num},res=>{
-                      callback(res);
-                    })
-                }else if(res.code == 201){
-                    App.popToast(res.msg)
-                }
-            })
+                },
+                fail: res => {
+                    App.popToast("订单支付失败")
+                },
+            });
+        };
+        App._get('v1_0_0.order/suborder', {
+            user_id: wx.getStorageSync('user_id'),
+            order_type: e.currentTarget.dataset.type,
+            food_id,
+            remark: e.currentTarget.dataset.type == 1 ? lunch_remark || '' : dinner_remark || ''
+        }, res => {
+            if (res.data.user_auth == 3) {
+                App.popToast(res.msg)
+                _this.setData({
+                    pop_show: false
+                })
+                wx.switchTab({
+                    url: '/pages/order/index'
+                })
+            } else if (res.data.user_auth == 1) {
+                App._post_form('v1_0_0.wxpay/unifiedorder', { order_num: res.data.order_num }, res => {
+                    callback(res);
+                })
+            } else if (res.code == 201) {
+                App.popToast(res.msg)
+            }
+        })
+    },
+    tab_btn(e){
+        let _this = this
+        _this.setData({
+            select_tabid:e.currentTarget.dataset.id
+        },res=>{
+            _this.orderList()
+        })
+    },
+    muslim_tab(e){
+        let _this = this
+        _this.setData({
+            select_muslimId:e.currentTarget.dataset.id
+        },res=>{
+            
+        })
     }
 })
